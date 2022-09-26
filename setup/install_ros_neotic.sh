fec4fa7227d34f60c448a90dbd6b3b097dc39b64
Cai Panpan <pcai2@e.ntu.edu.sg>
2022-09-26 16:42:21 +0800

    Create install_ros_neotic.sh

---
+++ setup/install_ros_neotic.sh
@@ -0,0 +1,14 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt install ros-noetic-desktop-full ros-neotic-navigation
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+sudo rosdep init
+rosdep update
