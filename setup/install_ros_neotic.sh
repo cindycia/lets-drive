a6444c3e110ebe64e04f5b713e034e578f5e812e
Cai Panpan <pcai2@e.ntu.edu.sg>
2022-09-26 16:47:41 +0800

    Update install_ros_neotic.sh

--- setup/install_ros_neotic.sh
+++ setup/install_ros_neotic.sh
@@ -3,7 +3,7 @@ sudo apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
-sudo apt install ros-noetic-desktop-full ros-neotic-navigation
+sudo apt install ros-noetic-desktop-full
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
