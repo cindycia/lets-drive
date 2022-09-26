582efc229e6b851aba37aa6679995a545e6dd96e
Cai Panpan <pcai2@e.ntu.edu.sg>
2022-09-26 16:57:55 +0800

    Update install_ros_neotic.sh

--- setup/install_ros_neotic.sh
+++ setup/install_ros_neotic.sh
@@ -12,3 +12,11 @@ sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator 
 
 sudo rosdep init
 rosdep update
+
+sudo sh \
+    -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" \
+    > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install python3-catkin-tools
