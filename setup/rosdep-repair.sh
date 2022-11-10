6159959988db0a4c6108eb509e971518eff005a4
Cai Panpan <pcai2@e.ntu.edu.sg>
2022-11-10 14:30:53 +0800

    Update rosdep-repair.sh

--- setup/rosdep-repair.sh
+++ setup/rosdep-repair.sh
@@ -1,55 +1,52 @@
 #!/usr/bin/env bash
-
+  
 # Ref: https://blog.csdn.net/leida_wt/article/details/115120940
 
 STR='https:\/\/raw.githubusercontent.com'
 GHPROXY='https:\/\/ghproxy.com\/'
 
-# 1
-
-sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
-	/etc/ros/rosdep/sources.list.d/20-default.list
-	
 if [ -f /usr/lib/python3/dist-packages/rosdep2/sources_list.py.bak ]; then
-	echo "PASS to EXIT!!!!!!"
-	exit 0
+        echo "PASS to EXIT!!!!!!"
+        exit 0
 else
 
-	sudo cp -rvf /usr/lib/python3/dist-packages/rosdep2/sources_list.py \
-	             /usr/lib/python3/dist-packages/rosdep2/sources_list.py.bak
-
-	# sudo sed -i "311s/^/        url=\"${GHPROXY}\"+url\n/" \
-	#	/usr/lib/python3/dist-packages/rosdep2/sources_list.py
+        sudo cp -rvf \
+                /etc/ros/rosdep/sources.list.d/20-default.list \
+                /etc/ros/rosdep/sources.list.d/20-default.list.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdistro/__init__.py \
+                /usr/lib/python3/dist-packages/rosdistro/__init__.py.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py \
+                /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdep2/sources_list.py \
+                /usr/lib/python3/dist-packages/rosdep2/sources_list.py.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdep2/rep3.py \
+                /usr/lib/python3/dist-packages/rosdep2/rep3.py.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py \
+                /usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py.bak
+        sudo cp -rvf \
+                /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py \
+                /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py.bak
 fi
 
 
-# 2
-sudo sed -i "68s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdistro/__init__.py
+# 1
 
-
-# 3
-sudo sed -i "34s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py
-
-
-# 4
-sudo sed -i "64s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdep2/sources_list.py
-
-
-# 5
-sudo sed -i "36s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdep2/rep3.py
-
-
-# 6
-sudo sed -i "68s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py
-
-sudo sed -i "119s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py
-
-# 7
-sudo sed -i "34s/https:\/\//${GHPROXY}/" \
-	/usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /etc/ros/rosdep/sources.list.d/20-default.list
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdistro/__init__.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdep2/sources_list.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdep2/rep3.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py
+sudo sed -i "s/${STR}/${GHPROXY}${STR}/g" \
+        /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py
